514f63c3ed8a84c66ba286918bb538fffef4e03f
Roman Jaquez <roman.jaquez@gmail.com>
2020-08-21 08:26:29 -0400

    created with codesandbox, now with the actual angular CLI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./../app/home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: "home"
+  },
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
